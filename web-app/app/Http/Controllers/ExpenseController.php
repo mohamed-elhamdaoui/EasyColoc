fad48c126c9c4a3e20cdfd41ce95d204ac2a6c8f
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-03-02 00:12:28 +0000

    feat: add store function to save new expenses

--- web-app/app/Http/Controllers/ExpenseController.php
+++ web-app/app/Http/Controllers/ExpenseController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Expense;
+use App\Models\Membership;
 
 class ExpenseController extends Controller
 {
-    //
+    public function store(Request $request, $colocationId)
+    {
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'amount' => 'required|numeric|min:0.01',
+            'expense_date' => 'required|date',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+
+        $membership = Membership::where('user_id', auth()->id())
+            ->where('colocation_id', $colocationId)
+            ->where('status', 'ACTIVE')
+            ->firstOrFail();
+
+        Expense::create([
+            'title' => $request->title,
+            'amount' => $request->amount,
+            'expense_date' => $request->expense_date,
+            'category_id' => $request->category_id,
+            'colocation_id' => $colocationId,
+            'membership_id' => $membership->id,
+        ]);
+
+
+        return redirect()->route('colocations.show', $colocationId)
+                         ->with('success', 'Dépense ajoutée avec succès ! ✅');
+    }
 }
